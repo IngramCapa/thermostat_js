8f5098d7e5bbc6dc5cc03b7314f19e10de9811ed
Raluca Ciucu <raluca.ciucu@gmail.com>
2019-12-13 12:06:17 +0000

    Add css and jquery files

---
+++ interface.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    var thermostat = new Thermostat();
+    updateTemperature();
+
+
+    $('#up').on('click', function () { // event listener
+        thermostat.up(); // update model
+        updateTemperature(); // update view
+    });
+
+    $('#down').on('click', function () { 
+        thermostat.down(); 
+        updateTemperature();
+    });
+
+
+    $('#reset').on('click', function () { 
+        thermostat.reset(); 
+
+        updateTemperature();
+    });
+
+    $('#psm-on').on('click', function () { 
+        thermostat.switchPsmOn(); 
+        $('#power-saving-status').text('on')
+        updateTemperature();
+        
+    });
+
+    $('#psm-off').on('click', function () { 
+        thermostat.switchPsmOff(); 
+        $('#power-saving-status').text('off')
+        updateTemperature();
+        
+    });
+
+
+    function updateTemperature() {
+        $('#temperature').text(thermostat.temperature);
+        $('#temperature').attr('class', thermostat.energyLevel());
+    };
+
+    
+
+
+});
+
